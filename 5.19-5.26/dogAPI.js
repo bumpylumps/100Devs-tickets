b012aa03eaea693b816d557989e89a7ddaf0e0f8
bumpyLumps <alexander.fulop.art@gmail.com>
2022-05-25 16:47:49 -0400

    built try/catch fetch, renamed file

--- 5.19-5.26/dogAPI.js
+++
@@ -1,7 +0,0 @@
-//Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
---
+++ 5.19-5.26/try.catch.js
@@ -0,0 +1,30 @@
+//Code 08
+// async function getACuteDogPhoto(){
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+// getACuteDogPhoto()
+
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+
+  try{
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+  }
+  
+  catch (err){
+    console.log(err)
+  }
+
+
+}
